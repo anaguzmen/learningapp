e8e2e823a9bad029e5987dc12ec332ee958f464f
Ana Maria Guzman <ana_guz_men@yahoo.com>
2025-01-30 08:58:02 -0500

    Adding MySql package

--- obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee2c43196a6ade22c1dab958a85dbc9c5f1de2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0165d7daa8c80c9e513fc41579151758d108a4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
